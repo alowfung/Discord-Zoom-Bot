412a63b8b34130b05f2ec88c9a5d6bb11f0a3c28
Alexander Low <alowfung@mail.sfsu.edu>
2022-01-14 16:53:33 -0800

    Simple test to send a singular message in Discord

---
+++ src/main/java/TestSendMessage.java
@@ -0,0 +1,9 @@
+//import DiscordHandlerTemp;
+
+public class TestSendMessage {
+    public static void main(String[] args){
+        TestMineplex.mainToBeCalled();
+        DiscordHandlerTemp.sendMessage("Hello try this");
+        TestMineplex.close();
+    }
+}
